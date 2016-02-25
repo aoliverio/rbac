9dd0e84776194ba17a1553dae0c42bd3dfc11abc
Antonio Oliverio <antonio.oliverio@gmail.com>
2016-02-25 14:48:42 +0100

    Edit AppController

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -4,7 +4,6 @@ namespace RBAC\Controller;
 
 use App\Controller\AppController as BaseController;
 
-class AppController extends BaseController
-{
-
+class AppController extends BaseController {
+    
 }
